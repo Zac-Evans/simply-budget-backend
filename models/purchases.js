5208066f1fa827f8872d37e9f425a970f98ef496
zac-evans <zacevanscoding@gmail.com>
2020-11-23 20:38:29 -0500

    updated purchases model

--- models/purchases.js
+++ models/purchases.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       category_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
           model: "budget_categories",
           key: "id",
