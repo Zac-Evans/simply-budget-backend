2a00693c4ada2eb708b12231b30978248c21bcd9
zac-evans <zacevanscoding@gmail.com>
2020-11-23 21:28:55 -0500

    test

--- migrations/20201107183345-create-purchases.js
+++ migrations/20201107183345-create-purchases.js
@@ -1,27 +1,28 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('purchases', {
+    await queryInterface.createTable("purchases", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        onDelete: "SET NULL",
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('purchases');
-  }
+    await queryInterface.dropTable("purchases");
+  },
 };
