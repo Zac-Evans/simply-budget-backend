1a25eacd19842fedb5919b322d81776561133667
zac-evans <zacevanscoding@gmail.com>
2020-11-23 20:49:26 -0500

    test

--- models/purchases.js
+++ models/purchases.js
@@ -10,11 +10,11 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       models.budget_categories.hasMany(purchases, {
         foreignKey: "id",
-        onDelete='SET NULL | NO ACTION'
+        onDelete: "SET NULL | NO ACTION",
       });
       purchases.belongsTo(models.budget_categories, {
         foreignKey: "category_id",
-        onDelete='SET NULL | NO ACTION'
+        onDelete: "SET NULL | NO ACTION",
       });
     }
   }
